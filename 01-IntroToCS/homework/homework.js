0bf727220db67178a33d7a871c27f5524c7a9423
RiosHectorM <rios.hector.martin@gmail.com>
2022-10-26 12:10:40 -0300

    HW 01 ok

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -2,14 +2,19 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
-
+  let binario = 0
+  return num.split("").reverse().map((bit, index) => (bit==="1")?binario += Math.pow(2,index):binario).at(-1)
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
-
+  let resto = ""
+  while (num>=2) {
+    resto += Math.floor(num%2)
+    num = num/2
+  }
+  return ((num>0)?resto += 1:resto).split("").reverse().join("")
 }
-
 
 module.exports = {
   BinarioADecimal,
